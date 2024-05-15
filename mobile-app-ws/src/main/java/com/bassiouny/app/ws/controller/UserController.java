78e1bbd783639437eb41e41bc0b2ccfa8715a5ef
Abdelrhman Fathy <abdelrhman.fathy@gmail.com>
2024-05-15 23:37:43 +0400

    user controller

---
+++ mobile-app-ws/src/main/java/com/bassiouny/app/ws/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.bassiouny.app.ws.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+	@GetMapping
+	public String getUser(){
+		return "get User";
+		
+	}
+	
+	@PostMapping
+	public String createUser() {
+		return "add user";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "update user";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "delete user";
+	}
+
+}
